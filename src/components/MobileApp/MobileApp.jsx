79efb6c1bcef47accc34330998b23b5514691117
D1mitrj <Dimitrj.Samoljak@gmail.com>
2020-09-08 13:04:33 +0200

    :construction: giving the donation Value to a state
    The State can now show the donationvalue on the page for testing.

--- src/components/MobileApp/MobileApp.jsx
+++ src/components/MobileApp/MobileApp.jsx
@@ -1,6 +1,7 @@
 import { hot } from 'react-hot-loader/root';
 import React, { PureComponent } from 'react';
 import { Input } from 'chayns-components';
+import './mobileApp.css';
 
 // mobile view change here.
 class MobileApp extends PureComponent {
@@ -8,27 +9,48 @@ class MobileApp extends PureComponent {
         super();
         this.state = {
             userName: 'Maik',
-            artistName: 'Picaso',
-            donationValue: 1,
+            artistName: 'Picasso',
+            donationValue: 0,
 
         };
         this.sendDonation = this.sendDonation.bind(this);
+        this.changeDonationValue = this.changeDonationValue.bind(this);
     }
 
+    // sets the firstname of the user to the welcome sentence.
     componentDidMount() {
         this.setState({
             userName: chayns.env.user.firstName,
         });
     }
 
+    // sends the Donation to the artist. On clicking at the button 'OK'.
     sendDonation() {
         const { donationValue } = this.state;
-        console.log(donationValue);
+
         chayns.dialog.alert('', 'Danke für deine Spende!');
+        if (donationValue === 0) {
+            chayns.dialog.alert('', 'Geben Sie bitte einen Betrag ein.');
+            document.querySelector('.donationInput').value = '';
+        }
+    }
+
+    // get the value from the input and checks if the input is a digit.
+    changeDonationValue(event) {
+        const re = /\d*/g;
+
+        // const matcharr = ...event.match(re); <---------
+        // document.querySelector('.donationInput').value = ;
+        if (re.test(event)) {
+            this.setState({
+                donationValue: event,
+            });
+        }
+        // console.log(event.match(re));
     }
 
     render() {
-        const { userName, artistName } = this.state;
+        const { userName, artistName, donationValue } = this.state;
         return (
             <div>
                 <h1>
@@ -37,18 +59,19 @@ class MobileApp extends PureComponent {
                 <h2>
                     {`Wie viel möchtest du an ${artistName} spenden?`}
                 </h2>
-                <div className="donationValue">
-                    <button className="button" type="button" onClick={() => console.log('5€')}>
+                <div className="donationValues">
+                    <button className="button btn" type="button" onClick={() => console.log('5€')}>
                         5€
                     </button>
-                    <button className="button" type="button" onClick={() => console.log('10€')}>
+                    <button className="button btn" type="button" onClick={() => console.log('10€')}>
                         10€
                     </button>
-                    <Input placeholder="Betrag"/>
-                    <button className="button" type="button" onClick={this.sendDonation}>
+                    <Input type={Number} className="donationInput" onChange={(event) => this.changeDonationValue(event)} placeholder="Betrag" />
+                    <button className="button btn" type="button" onClick={this.sendDonation}>
                         Ok
                     </button>
                 </div>
+                <p>{donationValue}</p>
             </div>
         );
     }
